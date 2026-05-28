184957bd4fe84e5ec9d267b4fdd1dbf144cc0f69
Den <sokold3@gmail.com>
2026-05-28 22:11:00 +0200

    Fix: build name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.scheday.mobile',
+  appId: 'app.scheday',
   appName: 'ScheDay',
   webDir: 'www',
   server: {
